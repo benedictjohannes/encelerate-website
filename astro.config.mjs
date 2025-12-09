1e772c91ac787f77551d1d9c06703a7c4a1e1b37
benedict <yappy.benedict@gmail.com>
2025-12-10 04:34:16 +0700

    initial website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+    site: 'https://encelerate.com',
+    integrations: [mdx(), sitemap()],
+    markdown: {
+        shikiConfig: {
+            themes: {
+                light: 'github-light-high-contrast',
+                dark: 'github-dark-high-contrast',
+            },
+        },
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+})
