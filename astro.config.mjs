7b3abad9e124715e0ad873e37be80980c2220718
benedict <yappy.benedict@gmail.com>
2025-12-28 08:11:46 +0700

    add astro-expressive-code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://encelerate.com',
-    integrations: [mdx(), sitemap()],
-    markdown: {
-        shikiConfig: {
-            themes: {
-                light: 'github-light-high-contrast',
-                dark: 'github-dark-high-contrast',
-            },
-        },
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
-})
+	site: "https://encelerate.com",
+	integrations: [expressiveCode(), mdx(), sitemap()],
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: "github-light-high-contrast",
+				dark: "github-dark-high-contrast",
+			},
+		},
+	},
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});
