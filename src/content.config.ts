2c0bf91d0832817c170e6f2675494a10488b53d3
benedict <yappy.benedict@gmail.com>
2026-03-20 20:30:21 +0700

    add private marker for articles

--- src/content.config.ts
+++ src/content.config.ts
@@ -11,6 +11,7 @@ const blog = defineCollection({
             description: z.string(),
             pubDate: z.coerce.date(),
             updatedDate: z.coerce.date().optional(),
+            private: z.boolean().optional(),
             heroImage: image().optional(),
         }),
 })
